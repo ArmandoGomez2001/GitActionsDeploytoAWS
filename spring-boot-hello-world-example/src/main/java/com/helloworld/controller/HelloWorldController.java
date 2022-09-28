60d4a94e8a828f5ebc4e6018661af1911962ddda
ArmandoGomez2001 <114522485+ArmandoGomez2001@users.noreply.github.com>
2022-09-28 07:40:57 -0700

    "Test"

--- spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
+++ spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
@@ -7,6 +7,6 @@ public class HelloWorldController
 @RequestMapping("/")
 public String hello() 
 {
-return "<h1> Congratulations. You have successfully deployed the sample Spring Boot Application. Hello </h1>";
+return "<h1> Congratulations. You have successfully deployed the sample Spring Boot Application. Armando Gomez </h1>";
 }
 }
